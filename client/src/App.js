1c8610d2b33fd27d1858c64571c76033843b62ab
kokominji <kmj6386@gmail.com>
2025-08-20 17:52:04 +0900

    feat: sever에서 받은 data 뿌리기

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  const getPersonData=async()=>{
+  const [personList, setPersonList] = useState([])
+  const getPersonData = async() => {
     alert('요청!')
     await axios.get('/users')
-    .then(response=> {
+    .then(response => {
       console.log(response)
+      console.log(response.data)
+      setPersonList(response.data)
     })
-    .catch(err=>{
+    .catch(err => {
       console.log(err)
     })
-    //백엔드 서버에 요청전송 - axios
+    //백엔드서버에 요청전송하기 - axios
   }
+  const result = personList.map(
+    data => (<div key = {data.no} className='person'>
+                <div>아이디: {data.person_id}</div>
+                <div>이름: {data.person_name}</div>
+                <div>나이: {data.age}</div>
+                <div>생일: {data.birthday}</div>
+            </div>)
+  )
+
   return (
     <div className="App">
-      <button onClick={getPersonData}>클릭</button>
+      <button onClick = {getPersonData}> 클릭</button>
+      {result}
     </div>
   );
 }
